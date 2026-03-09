bea838d8a41b10c76961870255d74d2219eed3bb
Javier Rodríguez Lavandeira <javirlcy@gmail.com>
2026-03-09 11:34:41 +0000

    VFX

--- FencingVR/Assets/JaviFolder/SoundBeforeDestroy.cs
+++ FencingVR/Assets/JaviFolder/SoundBeforeDestroy.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class SoundBeforeDestroy : MonoBehaviour
 {
     public AudioClip destroySound;
     public float volume = 1.0f;
 
+    public ParticleSystem vfx;
+
     public void PlaySound()
     {
+        vfx.Play();
         AudioSource.PlayClipAtPoint(destroySound, transform.position, volume);
     }
 }
